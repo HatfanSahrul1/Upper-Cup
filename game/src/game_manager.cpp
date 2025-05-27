49b95e32d8815300a232c0748bc58b32bc1bd5df
HatfanSahrul1 <hatfansahrul1@gmail.com>
2025-05-27 10:51:40 +0700

    style : fixing typo

--- game/src/game_manager.cpp
+++ game/src/game_manager.cpp
@@ -3,7 +3,7 @@
 namespace UpperCup::Game
 {
     GameManager* GameManager::instance = nullptr;
-    GameManager::GameManager* GetInstance()
+    GameManager* GameManager::GetInstance()
     {
         if(!instance){
             instance = new GameManager();
