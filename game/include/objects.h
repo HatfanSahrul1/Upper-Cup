ccf77c144b81ffb679c6111cf96184f145a2c9e4
HatfanSahrul1 <hatfansahrul1@gmail.com>
2025-05-28 10:41:23 +0700

    refactor : update cup header

--- game/include/objects.h
+++ game/include/objects.h
@@ -18,13 +18,27 @@ namespace UpperCup::Objects
     class Cup : public Renderable
     {
         public:
-            Cup();
+            Cup(Vector2 startPos);
 
             void Render() override;
 
+            void Floating(bool isJetpackActive);
+            Vector2 GetPosition() const;
+
         private:
             Vector2 position;
-            float width;
-            float height;
+            float velocityY = 0.0f;
+
+            const float gravity     = 500.0f;
+            const float lift        = -600.0f;
+            const float maxHeight   = 50.0f;
+            const float groundY     = 500.0f;
+            const float width       = 40.0f;
+            const float height      = 60.0f;
+    };
+
+    class Obstacle : public Renderable
+    {
+        public:
     };
 }
