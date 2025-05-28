fa77e989d260a74b37a9f9a407a5bfc0f3fee7f6
HatfanSahrul1 <hatfansahrul1@gmail.com>
2025-05-28 10:40:33 +0700

    test : creating funtion to alter cup y

--- game/src/game_manager.cpp
+++ game/src/game_manager.cpp
@@ -3,11 +3,25 @@
 namespace UpperCup::Game
 {
     GameManager* GameManager::instance = nullptr;
+
     GameManager* GameManager::GetInstance()
     {
         if(!instance){
             instance = new GameManager();
         }
         return instance;
+    }
+
+    GameManager::GameManager() : cup_({100, 300}){}
+
+    void GameManager::ProccesInput()
+    {
+        bool isJumping = IsKeyDown(KEY_SPACE);
+        cup_.Floating(isJumping);
+    }
+
+    void GameManager::RenderObjects()
+    {
+        cup_.Render();
     }
 }
