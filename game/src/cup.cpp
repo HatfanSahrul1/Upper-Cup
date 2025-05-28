91c6b88975e1768fb4cd2d2beb769a19db1f1a9c
HatfanSahrul1 <hatfansahrul1@gmail.com>
2025-05-28 10:41:05 +0700

    feat : create new render looks and some function to jumping

--- game/src/cup.cpp
+++ game/src/cup.cpp
@@ -2,11 +2,52 @@
 
 namespace UpperCup::Objects
 {
+    Cup::Cup(Vector2 startPos) : position(startPos) {}
+
     void Cup::Render()
     {
-        DrawRectangleV(position, {width, height}, DARKBLUE);
+        Rectangle cupBody = {
+            position.x - width/2, 
+            position.y - height/2, 
+            width, 
+            height
+        };
+        DrawRectangle(cupBody.x, cupBody.y, cupBody.width, cupBody.height, GRAY);
+        DrawRectangleLines(cupBody.x, cupBody.y, cupBody.width, cupBody.height, BLACK);
+        
+        Rectangle handle = {
+            position.x + width/2, 
+            position.y - 10, 
+            8, 
+            20
+        };
+        DrawRectangleLines(handle.x, handle.y, handle.width, handle.height, BLACK);
+    }
 
-        Vector2 handlePos = {position.x + width, position.y + height / 4};
-        DrawCircleV(handlePos, height / 4, BLUE);
+    void Cup::Floating(bool isJetpackActive)
+    {
+        float dt = GetFrameTime();
+
+        if (isJetpackActive) {
+            velocityY += lift * dt;
+        } else {
+            velocityY += gravity * dt;
+        }
+
+        position.y += velocityY * dt;
+
+        // Batas atas & bawah
+        if (position.y < maxHeight) {
+            position.y = maxHeight;
+            velocityY = 0;
+        } else if (position.y > groundY) {
+            position.y = groundY;
+            velocityY = 0;
+        }
+    }
+
+    
+    Vector2 Cup::GetPosition() const {
+        return position;
     }
 }
