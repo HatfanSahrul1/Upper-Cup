508843ac97a23cc0169e6ce352ce7c687578b946
HatfanSahrul1 <hatfansahrul1@gmail.com>
2025-05-29 22:38:36 +0700

    feat : create obstacle and wall factory

--- game/include/factories.h
+++ game/include/factories.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <memory>
 #include "objects.h"
+#include <stdexcept>
 
 namespace UpperCup::Factories
 {
@@ -13,6 +14,39 @@ namespace UpperCup::Factories
             {
                 auto cup = std::make_shared<UpperCup::Objects::Cup>(position);
                 return cup;
+            }
+    };
+
+    class ObstacleFactory
+    {
+        public:
+            static std::shared_ptr<UpperCup::Objects::Obstacle> CreateObstacle(Vector2 initPos, float length, float distance)
+            {
+                auto obstacle = std::make_shared<UpperCup::Objects::Obstacle>(initPos, length, distance);
+                return obstacle;
+            }  
+    };
+
+    class WallFactory
+    {
+        public:
+            static std::shared_ptr<UpperCup::Objects::Wall> CreateWall(const std::string& type, float screenHeight)
+            {
+                if (type == "top") {
+                    Vector2 position = {0, 0}; 
+                    float height = 30.0f;
+                    auto wall = std::make_shared<UpperCup::Objects::Wall>(position, height);
+                    return wall;
+                }
+                else if (type == "bottom") {
+                    float groundHeight = 50.0f;
+                    Vector2 position = {0, screenHeight - groundHeight};
+                    auto wall = std::make_shared<UpperCup::Objects::Wall>(position, groundHeight);
+                    return wall;
+                }
+                else {
+                    throw std::invalid_argument("Invalid wall type: " + type);
+                }
             }
     };
 }
