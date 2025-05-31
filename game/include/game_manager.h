329939207651492cd53e9bca4333d4f155c5602f
HatfanSahrul1 <hatfansahrul1@gmail.com>
2025-06-01 02:50:44 +0700

    feat : adding obstacle manager to game manager and create it getter

--- game/include/game_manager.h
+++ game/include/game_manager.h
@@ -5,6 +5,7 @@
 #include "game_state.h"
 #include "objects.h"
 #include "factories.h"
+#include "obstacle_manager.h"
 
 using namespace UpperCup;
 
@@ -22,9 +23,12 @@ namespace UpperCup::Game
 
             std::shared_ptr<Objects::Cup> GetCup();
             Camera2D* GetCamera();
+            void ResetCamera();
 
             void SetScore(int& i);
             int GetScore();
+            
+            ObstacleManager* GetObstacleManager();
         private:
             static GameManager* instance;
             GameManager();
@@ -33,9 +37,10 @@ namespace UpperCup::Game
 
             Camera2D camera_ = {0};
 
+            ObstacleManager obs_manager_;
+
             std::shared_ptr<IGameState> currentState_ = nullptr;
             std::shared_ptr<Objects::Cup> cup_;
-            std::shared_ptr<Objects::Obstacle> obs_;
             std::shared_ptr<Objects::Wall> top_, bottom_;
     };
 }
