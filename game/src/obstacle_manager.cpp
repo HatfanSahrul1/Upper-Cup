07c83ba6e10bfced1fc6674bf47cca27c97875cc
HatfanSahrul1 <hatfansahrul1@gmail.com>
2025-06-01 02:48:20 +0700

    feat : create Create and Render function on vector of obstacles

---
+++ game/src/obstacle_manager.cpp
@@ -0,0 +1,33 @@
+#include "obstacle_manager.h"
+
+namespace UpperCup::Game
+{
+    void ObstacleManager::CreateObstacle()
+    {
+        obstacles_.clear();
+
+        for(int i = 0; i < obs_init_.size(); i ++){
+            if(i == 0){
+                Vector2 pos = {GetScreenWidth() + obs_init_[i].pos.x, obs_init_[i].pos.y};
+                auto obs = Factories::ObstacleFactory::CreateObstacle(pos, obs_init_[i].length, obs_init_[i].dist);
+                obstacles_.push_back(obs);
+            }
+            else
+            {
+                Vector2 pos = {obs_init_[i].dist + obs_init_[i-1].pos.x, obs_init_[i].pos.y};
+                auto obs = Factories::ObstacleFactory::CreateObstacle(pos, obs_init_[i].length, obs_init_[i].dist);
+                obstacles_.push_back(obs);
+            }
+        }
+    }
+
+    void ObstacleManager::RenderObstacles()
+    {
+        if(obstacles_.empty()) return;
+
+        for(auto obj : obstacles_)
+        {
+            obj->Render();
+        }
+    }
+}
