9617b30fc0463c2466d2f45cd46dfe8cd733db16
HatfanSahrul1 <hatfansahrul1@gmail.com>
2025-05-31 17:00:36 +0700

    style : change several text and remove whitespaces

--- game/src/home_screen.cpp
+++ game/src/home_screen.cpp
@@ -5,12 +5,10 @@
 namespace UpperCup::Game
 {
     bool HomeScreen::Enter() {
-        // Inisialisasi jika diperlukan
         return true;
     }
 
     bool HomeScreen::MainState() {
-        // Cek input untuk berpindah ke Playing
         if (IsKeyPressed(KEY_SPACE)) {
             GameManager::GetInstance()->ChangeState(std::make_shared<Playing>());
         }
@@ -22,8 +20,7 @@ namespace UpperCup::Game
     }
 
     void HomeScreen::Render() {
-        // Gambar judul dan instruksi
         DrawText("UPPERCUP", 200, 200, 60, BLACK);
-        DrawText("Press SPACE to Play", 200, 300, 20, DARKGRAY);
+        DrawText("Press SPACE to Jump & Play", 200, 300, 20, DARKGRAY);
     }
 }
